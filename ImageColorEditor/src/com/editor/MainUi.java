4d3b105bf9c911326b0aa81c3166b554bc8b70c0
kiranfl <kiran.rajam@fissionlabs.in>
2015-12-09 13:19:40 +0530

    sample commit

--- ImageColorEditor/src/com/editor/MainUi.java
+++ ImageColorEditor/src/com/editor/MainUi.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.dnd.DropTarget;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
