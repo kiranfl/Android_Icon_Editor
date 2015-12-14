640ac883b155180fca3915d57029a4b297ec4aee
kiranfl <kiran.rajam@fissionlabs.in>
2015-12-14 15:16:37 +0530

    Added Icon resize functionality

--- ImageColorEditor/src/com/editor/DragAndDropListener.java
+++ ImageColorEditor/src/com/editor/DragAndDropListener.java
@@ -12,49 +12,49 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class DragAndDropListener implements DropTargetListener{
+public class DragAndDropListener implements DropTargetListener {
 
-	@Override
-	public void dragEnter(DropTargetDragEvent dtde) {
-		// TODO Auto-generated method stub
-		System.out.println("dragEnter");
-	}
+    @Override
+    public void dragEnter(DropTargetDragEvent dtde) {
+        // TODO Auto-generated method stub
+        System.out.println("dragEnter");
+    }
 
-	@Override
-	public void dragExit(DropTargetEvent dte) {
-		// TODO Auto-generated method stub
-		System.out.println("dragExit");
-	}
+    @Override
+    public void dragExit(DropTargetEvent dte) {
+        // TODO Auto-generated method stub
+        System.out.println("dragExit");
+    }
 
-	@Override
-	public void dragOver(DropTargetDragEvent dtde) {
-		// TODO Auto-generated method stub
-		System.out.println("dragOver");
-	}
+    @Override
+    public void dragOver(DropTargetDragEvent dtde) {
+        // TODO Auto-generated method stub
+        System.out.println("dragOver");
+    }
 
-	@Override
-	public void drop(DropTargetDropEvent dtde) {
-		// TODO Auto-generated method stub
-		dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
-		Transferable t = dtde.getTransferable();
-		try {
-			@SuppressWarnings("unchecked")
-			List<File> files = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
-			if (files.size() == 1) {
+    @Override
+    public void drop(DropTargetDropEvent dtde) {
+        // TODO Auto-generated method stub
+        dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+        Transferable t = dtde.getTransferable();
+        try {
+            @SuppressWarnings("unchecked")
+            List<File> files = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
+            if (files.size() == 1) {
                 MainUi.getInstance().setImageIcon(files.get(0));
+            }
+        } catch (UnsupportedFlavorException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-		} catch (UnsupportedFlavorException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-         
-		System.out.println("drop");
-	}
 
-	@Override
-	public void dropActionChanged(DropTargetDragEvent dtde) {
-		// TODO Auto-generated method stub
-		System.out.println("dropActionChanged");
-	}
+        System.out.println("drop");
+    }
+
+    @Override
+    public void dropActionChanged(DropTargetDragEvent dtde) {
+        // TODO Auto-generated method stub
+        System.out.println("dropActionChanged");
+    }
 
 }
